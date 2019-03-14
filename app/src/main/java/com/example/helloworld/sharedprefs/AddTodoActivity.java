0b19a31fb0edca3fcf4f03aed3519e291c837184
Manik1094 <guptamanik1999@gmail.com>
2019-03-15 02:30:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/sharedprefs/AddTodoActivity.java
@@ -0,0 +1,69 @@
+package com.example.helloworld.sharedprefs;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class AddTodoActivity extends AppCompatActivity {
+
+    EditText title_et , id_et;
+    Button add_todo_btn;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private boolean priority = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_todo);
+
+        preferences = getApplicationContext().getSharedPreferences("TodoPrefs" , MODE_PRIVATE);
+        editor = preferences.edit();
+
+        title_et = findViewById(R.id.title_et);
+        id_et = findViewById(R.id.id_et);
+        add_todo_btn = findViewById(R.id.add_todo_btn);
+
+        add_todo_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                addTodoToPrefs();
+                finish();
+            }
+        });
+    }
+
+    private void addTodoToPrefs() {
+
+        editor.putString("title" , title_et.getText().toString().trim());
+        editor.putBoolean("priority" , priority);
+        editor.putInt("id" , Integer.parseInt(id_et.getText().toString()));
+
+        editor.commit();
+
+    }
+
+    public void onRadioButtonClicked(View view){
+        boolean isChecked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()){
+            case R.id.low_rb : if(isChecked){
+
+                                   priority = false;
+                                    }
+                                break;
+
+            case R.id.high_rb : if(isChecked){
+
+                priority = true;
+            }
+            break;
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/helloworld/sharedprefs/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.helloworld.sharedprefs;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button add_btn;
+    TextView title_txt , priority_txt , id_txt;
+    SharedPreferences preferences;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        add_btn = findViewById(R.id.add_btn);
+        title_txt = findViewById(R.id.title_txt);
+        priority_txt = findViewById(R.id.priority_txt);
+        id_txt = findViewById(R.id.id_txt);
+
+
+
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , AddTodoActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        preferences = getApplicationContext().getSharedPreferences("TodoPrefs",MODE_PRIVATE);
+
+        retreiveData();
+    }
+
+    private void retreiveData() {
+
+        String title = preferences.getString("title" , "none");
+        int id = preferences.getInt("id" , 0);
+        boolean priority = preferences.getBoolean("priority" , false);
+
+        title_txt.setText("Title : " + title);
+        id_txt.setText("Id : " + id);
+        if(priority){
+            priority_txt.setText("Priority : High" );
+        }else {
+            priority_txt.setText("Priority : Low");
+        }
+
+    }
+}
